a66bf71b8631f35d07db29d424b40882e47479da
asif-fahad <asif123fahad@gmail.comm>
2023-04-11 04:03:53 +0600

    more css added

--- src/AppliedJob.jsx
+++ src/AppliedJob.jsx
@@ -8,35 +8,39 @@ const AppliedJob = ({ c }) => {
     return (
         <div>
 
-            <div className='flex border w-9/12 mx-auto flex-'>
-                <img src={logo} alt="" className='object-contain w-36' />
-                <div className='review-details'>
-                    <h1 className="font-bold">{job_title}</h1>
-                    <h1 className="font-bold">{company_name}</h1>
-                    <div className='flex gap-4'>
-                        <span className="border">{remote_onsite}</span>
-                        <span className="border">{fulltime_parttime}</span>
-                    </div>
-                    <div className='flex gap-4'>
-                        <div className='flex'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                            </svg>
-                            <span>{location}</span>
+            <div className='flex border w-full lg:w-8/12 mx-auto mt-5'>
+                <div className="ms-4">
+                    <img src={logo} alt="" className='object-contain w-36 h-36' />
+                    <div className='review-details'>
+                        <h1 className="font-bold">{job_title}</h1>
+                        <h1 className="font-bold">{company_name}</h1>
+                        <div className='flex gap-4'>
+                            <span className="border">{remote_onsite}</span>
+                            <span className="border">{fulltime_parttime}</span>
                         </div>
+                        <div className='flex gap-4'>
+                            <div className='flex'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                </svg>
+                                <span>{location}</span>
+                            </div>
 
-                        <div className='flex'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                            <span>{salary}</span>
+                            <div className='flex'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <span>{salary}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <Link to={`/jobDetails/${id}`} >
-                    <button className='flex-grow font-medium  transition duration-200  shadow-md  md:mb-0  px-4 py-2 md:px-8 md:py-3 m-2 text-lg rounded-full border-transparent border-2  text-gray-700 hover:bg-cyan-400 bg-cyan-200'>View Details</button>
-                </Link>
+                <div className="flex justify-center items-center flex-grow">
+                    <Link to={`/jobDetails/${id}`} >
+                        <button className='flex-grow font-medium  transition duration-200  shadow-md  md:mb-0  px-4 py-2 md:px-8 md:py-3 m-2 text-lg rounded-full border-transparent border-2  text-gray-700 hover:bg-cyan-400 bg-cyan-200'>View Details</button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
--- src/AppliedJobs.jsx
+++ src/AppliedJobs.jsx
@@ -17,7 +17,7 @@ const AppliedJobs = () => {
     return (
         <div>
             <h1 className='text-4xl text-center m-10 bg-gray-200 p-10'>Applied Jobs</h1>
-            <div className='flex flex-col justify-end items-end max-w-xs'>
+            <div className='flex flex-col justify-end items-end'>
                 <button onClick={handleOnsite} className='flex-grow font-medium  transition duration-200  shadow-md  md:mb-0  px-4 py-2 md:px-8 md:py-3 m-2 text-lg rounded-full border-transparent border-2  text-gray-700 hover:bg-cyan-400 bg-cyan-200'>Onsite</button>
                 <button onClick={handleRemote} className='flex-grow font-medium  transition duration-200  shadow-md  md:mb-0  px-4 py-2 md:px-8 md:py-3 m-2 text-lg rounded-full border-transparent border-2  text-gray-700 hover:bg-cyan-400 bg-cyan-200'>Remote</button>
             </div>
